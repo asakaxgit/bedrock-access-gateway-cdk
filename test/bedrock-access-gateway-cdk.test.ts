f73e562de3f9bf5e34d78a6407ee30df4d1900ad
A. Sakashita <ats@saks.industries>
2025-04-26 03:30:50 +0000

    Initial commit

---
+++ test/bedrock-access-gateway-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BedrockAccessGatewayCdk from '../lib/bedrock-access-gateway-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bedrock-access-gateway-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BedrockAccessGatewayCdk.BedrockAccessGatewayCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
